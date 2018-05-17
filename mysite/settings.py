1ac52190fa2b06600c793d428bc2a35d4033c4f2
Daniel Burke <daniel.burke@magellangroup.net>
2018-05-17 12:54:36 +0100

    Comp Tut01

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+##print ('BASE_DIR', BASE_DIR)
+##print ('TEMPLATE_DIRS', TEMPLATE_DIRS)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=128)
     pub_date = models.DateTimeField('date published')
@@ -11,6 +10,9 @@ class Question(models.Model):
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        was_published_recently.admin_order_field = 'pub_date'
+        was_published_recently.boolean = True
+        was_published_recently.short_description = 'published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey('Question', on_delete=models.CASCADE)
