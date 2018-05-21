8b9c602d730b126578ec6ca8efaf8070ee5e29f3
Daniel Burke <daniel.burke@magellangroup.net>
2018-05-21 14:42:47 +0100

    fin Tut04

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('polls/', include('polls.urls', namespace='polls')),
     path('admin/', admin.site.urls),
 ]
--- polls/urls.py
+++ polls/urls.py
@@ -1,23 +1,23 @@
 from django.conf.urls import include, url
-from django.contrib import admin
+
 from polls import views
 
-##app_name = "polls"
+app_name = 'polls'
 
 urlpatterns = [
-        # ex: /polls/
-        url(r'^$', views.index, name='index'),
-        # ex: /polls/5/
-        url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-        # ex: /polls/5/results
-        url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
-        # ex: /polls/5/vote
-        url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
-       ## url(r'^polls/', include('polls.urls')),
-       ## url(r'^admin/', admin.site.urls),
+            url(r'^$', views.IndexView.as_view(), name='index'),
+            url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+            url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+            url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 ]
 
 '''
 url(r'^about/', views.about, name='about'),
 url(r'^category/(?P<category_name_slug>[\w-]+)/$', views.category, name='category'),
+##url(r'^polls/', include('polls.urls', namespace='polls')), 
+##url(r'^admin/', admin.site.urls),
+
+from django.urls import path
+from django.contrib import admin
+admin.autodiscover()
 '''
