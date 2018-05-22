169ed530d9dfbc1a53cb3a6025045ad44a277746
Daniel Burke <daniel.burke@magellangroup.net>
2018-05-22 14:42:02 +0100

    fin Tut05

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 from polls.models import Choice, Question
 
@@ -9,12 +10,16 @@ class IndexView(generic.ListView):
     context_object_name =  'latest_question_list'
     
     def get_queryset(self):
-        #Return the last 5 published Questions
-        return Question.objects.order_by('pub_date')[:5]
+        return Question.objects.filter(
+                pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5] #Return the last 5 published Questions
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self): # Excludes any questions that aren't published yet.
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -33,10 +38,6 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a user hits the Back button.
+        # Always return an HttpResponseRedirect after successfully dealing with POST data. This prevents data from being posted twice if a user hits the Back button.
     return HttpResponseRedirect(reverse('polls:results',  args=(p.id,)))
 
-'''
-, HttpResponse, Http404
-'''
